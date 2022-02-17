4b70ea4f6df04c45181e336fe09223eeb98e3bb9
Gabriel Milano <milano.developer@gmail.com>
2022-02-16 22:32:01 -0300

    First commit starting project

---
+++ src/main/java/com/changym/demo/ChanGymApplication.java
@@ -0,0 +1,13 @@
+package com.changym.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changym/demo/ChanGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changym.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
